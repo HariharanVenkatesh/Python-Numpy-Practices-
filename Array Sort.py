45e8b917299fb8dfccde545edee02cb94fd897ca
HariharanVenkatesh <harihaniscristo23@gmail.com>
2024-09-12 16:08:09 +0530

    Array Sorting

---
+++ Array Sort.py
@@ -0,0 +1,7 @@
+import numpy as jk
+# arr = jk.array([3, 2, 0, 1])
+# arr2 = jk.array(['Banana', 'Cherry', 'Apple'])
+# arr3 = jk.array([True, False, True])
+# arr4 = jk.array([[3, 2, 4],[5, 0, 1]])
+print(jk.sort) 
+
